d7a0fa7a5690f2841fa701afebb785d68528667c
Yuting Liu <83076528+yeziver@users.noreply.github.com>
2022-09-19 09:07:29 -0400

    Update 26-remove_duplicates.cpp

--- 26-remove_duplicates.cpp
+++ 26-remove_duplicates.cpp
@@ -5,6 +5,46 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        for (int j = 1; j < nums.size(); j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j]; 
+            }
+        }
+        return i+1;
+    }
+};
+
+////
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int l = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i-1]) {
+                nums[l++] = nums[i];
+            }
+        }
+        return l;
+    }
+};
+
+
+//USING BUILT IN FUNCTIONS
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
         std::sort(nums.begin(), nums.end());
         nums.erase(std::unique(nums.begin(), nums.end()), nums.end());
         return nums.size();
