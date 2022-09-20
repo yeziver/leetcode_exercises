281c11fcffed1bf99dbd94f19aa7283c78ee0d1b
Yuting Liu <83076528+yeziver@users.noreply.github.com>
2022-09-19 20:43:24 -0400

    Update 136-single_number.cpp

--- 136-single_number.cpp
+++ 136-single_number.cpp
@@ -1,3 +1,5 @@
+//USING MAP
+
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
@@ -12,4 +14,20 @@ public:
         }
         return -1;
         }
+};
+
+
+//LOOPING
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); i+=2) {
+            if (nums[i] != nums[i-1]) {
+                return nums[i-1];
+            }
+        }
+        return nums[nums.size()-1];
+    }
 };
