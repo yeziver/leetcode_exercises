2f8998efb0c61913c97437e59c182277996b289a
Yuting Liu <83076528+yeziver@users.noreply.github.com>
2022-09-19 20:33:37 -0400

    Update 136-single_number.cpp

--- 136-single_number.cpp
+++ 136-single_number.cpp
@@ -1,20 +1,15 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        auto it = unique(nums.begin(), nums.end()); // doesn't work because it only checks if there are duplicates not 
-        bool wasUnique;
-        for (int i = 0; i < nums.size(); i++) {
-            wasUnique = (it == nums.end());
-            if (wasUnique) {
-                return nums[i];
-                continue;
-            } 
+        unordered_map<int, int> freqs;
+        for (auto x: nums) {
+            freqs[x]++;
         }
-        return 0;
-    }
+        for (auto z: freqs) {
+            if (z.second == 1) {
+                return z.first;
+            }
+        }
+        return -1;
+        }
 };
