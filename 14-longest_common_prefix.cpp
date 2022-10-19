83572b167a7c2f54ba94a8f37a07cb8386612b49
Yuting Liu <83076528+kafka-ots@users.noreply.github.com>
2022-10-19 16:08:45 -0400

    Update 14-longest_common_prefix.cpp

--- 14-longest_common_prefix.cpp
+++ 14-longest_common_prefix.cpp
@@ -1,24 +1,18 @@
+
+
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) { 
-        if (strs.size() == 1) {
-            return strs[0];
-        }
-        int count = 0;
-        string result = "";
-        for (int i = 0; i < strs.size(); i++) {
-            for (int j = i+1; j < strs.size(); j++) {
-                string temp = strs[i];
-                string temp2 = strs[j];
-                if (temp[count] == temp2[count]) {
-                    result.push_back(strs[i][count]);
-                    count++;
-                } else {
-                    return result;;
-                }
+string longestCommonPrefix(vector<string>& strs) {
+    string result = "";
+    string first = strs[0];
+    for (int i = 0; i < first.size(); i++) {
+        for (int j = 1; j < strs.size(); j++) {
+            string temp = strs[j];
+            if (temp[i] != first[i]) {
+                return result;
             }
-            count++;
         }
-        return result;
+        result += first[i];
     }
-};
+    return result;
+}};
