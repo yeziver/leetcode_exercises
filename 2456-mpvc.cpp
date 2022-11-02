394430c4263ac4d57444b94b4a7ac88f4f915ae1
Yuting Liu <83076528+kafka-ots@users.noreply.github.com>
2022-11-02 12:39:12 -0400

    Create 2456-mpvc.cpp

---
+++ 2456-mpvc.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> mostPopularCreator(vector<string>& creators, vector<string>& ids, vector<int>& views) {
+        vector<int> highestViewsIndex;
+        int max = 0;
+        for (int i = 0; i < views.size(); i++) {
+            if (view[i] > max) {
+                max = view[i];
+            }
+        }
+        for (int i = 0; i < views.size(); i++) {
+            if (view[i] == max) {
+                highestViewsIndex.push_back(i);
+            }
+        }
+    }
+};
